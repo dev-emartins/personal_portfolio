5ca651fd6d2329f92787a7b1eb39e60d99832fda
dev-emartins <everaldoinfortecnico@gmail.com>
2025-07-20 20:57:33 -0300

    feat:creation of the footer with information about the current year and copyright.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 
 import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <main className="w-full flex-1">
         <Outlet />
       </main>
-      {/* Footer */}
+      <Footer />
     </>
   )
 }
